c70d5fda251c6fac491f7e7b5c4fac41dcbb1bd6
LorenzoGuidi <guidilorenzo96@gmail.com>
2023-09-29 22:13:21 +0200

    2 ricerca

--- src/main/java/com/example/mensa/service/AlunnoSpecification.java
+++ src/main/java/com/example/mensa/service/AlunnoSpecification.java
@@ -15,13 +15,22 @@ public class AlunnoSpecification {
             Predicate predicateCodiceFiscale = criteriaBuilder.like(root.get("codiceFiscale"), "%" + codiceFiscale.toLowerCase() + "%");
             
             Predicate predicateSezioneScuola = criteriaBuilder.like(root.get("sezioneScuola"), "%" + codiceFiscale.toLowerCase() + "%");
-            Predicate predicateAnnoAccademico = criteriaBuilder.equal(root.get("annoAccademico"), annoAccademico);
-            Predicate predicateCognomeBambino = criteriaBuilder.like(root.get("cognome") , "%" + codiceFiscale.toLowerCase() + "%");
-            Predicate predicateCognomeRichiedente = criteriaBuilder.like(root.get("cognomeRichidente") , "%" + codiceFiscale.toLowerCase() + "%");
+            Predicate predicateAnnoAccademico = criteriaBuilder.or(
+                    criteriaBuilder.equal(root.get("annoAccademico"), annoAccademico),
+                    criteriaBuilder.isNull(root.get("annoAccademico"))
+            );
+            Predicate predicateCognomeBambino = criteriaBuilder.like(root.get("cognome"), "%" + codiceFiscale.toLowerCase() + "%");
+            Predicate predicateCognomeRichiedente = criteriaBuilder.like(root.get("cognomeRichidente"), "%" + codiceFiscale.toLowerCase() + "%");
 
-            
-            Predicate predicateOR = criteriaBuilder.or(predicateCodiceFiscale, predicateSezioneScuola , predicateCognomeBambino, predicateCognomeRichiedente);
-            
+            // Combina le condizioni utilizzando OR
+            Predicate predicateOR = criteriaBuilder.or(
+                    predicateCodiceFiscale, 
+                    predicateSezioneScuola, 
+                    predicateCognomeBambino, 
+                    predicateCognomeRichiedente
+            );
+
+            // Combina il predicato OR con la condizione sull'anno accademico usando AND
             return criteriaBuilder.and(predicateOR, predicateAnnoAccademico);
         };
     }
